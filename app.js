f0e4b95afd24c2bc2bcc2c104309c6b36f41d6d5
mahbubrahman1 <mahbuburrofficial@gmail.com>
2022-03-02 17:00:59 +0600

    finished

--- app.js
+++ app.js
@@ -12,6 +12,28 @@ function updateProductAmount(product, perProductPrice, isIncrease) {
     // update total amount
     const productTotal = document.getElementById(product + '-total');
     productTotal.innerText = productNumber * perProductPrice;
+
+    // calculate total
+    calculateTotal();
+}
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1399;
+    const caseTotal = getInputValue('case') * 29;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 5;
+    const totalPrice = subTotal + tax;
+
+    // update on bottom
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
 }
 
 document.getElementById('phone-plus').addEventListener('click', function () {
